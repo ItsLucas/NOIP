04dcc68874e77018d232c31acd5fda058a359802
ItsLucas <itslucas@itslucas.me>
2017-08-16 10:38:55 +0800

    update brike.cpp

--- sim3/shtsc-34/brike/brike.cpp
+++ sim3/shtsc-34/brike/brike.cpp
@@ -30,14 +30,21 @@ int main() {
     memset(f, -1, sizeof(f));
     for (int i = 1; i <= n; i++)
         f[i][0][0] = 0, f[i][1][1] = a[i][1];
-    for (int i = 1; i <= n; i++) {
-        for (int j = 0; j <= i; j++) {
-            for (int k = 0; k <= m; k++) {
+    for (int i = 1; i <= n; i++) {         // the ith line
+        for (int j = 0; j <= i; j++) {     // got j bricks
+            for (int k = 0; k <= m; k++) { // k bricks in total
                 if (j <= k) {
-                    for (int p = j - 1 >= 0 ? j - 1 : 0; p <= i - 1; p++) {
+                    int p;
+                    if (j - 1 >= 0) {
+                        p = j - 1;
+                    } else {
+                        p = 0;
+                    }
+                    for (p; p <= i - 1; p++) {
                         if (f[i - 1][p][k - j] != -1) {
                             f[i][j][k] =
                                 max(f[i][j][k], f[i - 1][p][k - j] + sum[i][j]);
+                            // f[i][j][k]=max{f[i][j][k-j],f[i][j][k]+sum[i][j]
                         }
                     }
                 }
