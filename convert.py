625b850dd42f5f5cc3ad0aa99d9f4a382314cfd7
ItsLucas <vgrl@vgrl.me>
2017-10-05 15:41:18 +0800

    update

--- convert.py
+++
@@ -1,5 +0,0 @@
-import Image
-
-img = Image.open("./MathPic.ppm")
-img.save("./MathPic.jpg")
-img.show()
--- wp7.cpp
+++ wp7.cpp
@@ -30,11 +30,11 @@ void pixel_write(int i, int j) {
 }
 unsigned char RD(int i, int j) {
     double a = 0, b = 0, c, d, n = 0;
-    while ((c = a * a) + (d = b * b) < 4 && n++ < 880) {
+    while ((c = a * a) + (d = b * b) < 4 && n++ < 880) { // 880
         b = 2 * a * b + j * 8e-9 - .645411;
         a = c - d + i * 8e-9 + .356888;
     }
-    return 255 * pow((n - 80) / 800, 3.);
+    return 255 * pow((n - 80) / 800, 3.); // 3.
 }
 unsigned char GR(int i, int j) {
     double a = 0, b = 0, c, d, n = 0;
@@ -42,7 +42,7 @@ unsigned char GR(int i, int j) {
         b = 2 * a * b + j * 8e-9 - .645411;
         a = c - d + i * 8e-9 + .356888;
     }
-    return 255 * pow((n - 80) / 800, .7);
+    return 255 * pow((n - 80) / 800, .45); //.7
 }
 unsigned char BL(int i, int j) {
     double a = 0, b = 0, c, d, n = 0;
@@ -50,5 +50,5 @@ unsigned char BL(int i, int j) {
         b = 2 * a * b + j * 8e-9 - .645411;
         a = c - d + i * 8e-9 + .356888;
     }
-    return 255 * pow((n - 80) / 800, .5);
+    return 255 * pow((n - 80) / 800, .9); //.5
 }
