d063c4be1feca98a16bf4f810978ebd6c58ed091
ItsLucas <vgrl@vgrl.me>
2018-11-24 21:49:27 +0800

    123123

--- wp7.cpp
+++ wp7.cpp
@@ -32,7 +32,7 @@ unsigned char RD(int i, int j) {
     double a = 0, b = 0, c, d, n = 0;
     while ((c = a * a) + (d = b * b) < 4 && n++ < 880) { // 880
         b = 2 * a * b + j * 8e-9 - .645411;
-        a = c - d + i * 8e-9 + .356888;
+        a = c - d + i * 8e-9 + .356878;
     }
     return 255 * pow((n - 80) / 800, 3.); // 3.
 }
